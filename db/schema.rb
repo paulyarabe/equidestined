5affd8755e046cc244e32ed1a77b349c6a827b95
github username <github email address>
2017-07-13 14:12:41 -0400

    added user login and sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170712193944) do
-=======
 ActiveRecord::Schema.define(version: 20170713012109) do
->>>>>>> User
 
   create_table "locations", force: :cascade do |t|
     t.float "latitude"
