c0fe05b42d08ee1e6db09f61d049aa94cf20f80a
github username <github email address>
2017-07-12 21:47:52 -0400

    fixed users, sign up, login, to include what we had before everything crashed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710121950) do
+ActiveRecord::Schema.define(version: 20170713012109) do
 
   create_table "locations", force: :cascade do |t|
     t.float "latitude"
