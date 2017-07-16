559eac818afb8a615a75d6cf96ec41ac7d0531fb
github username <github email address>
2017-07-16 14:41:10 -0400

    about page first draft done

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: 'login'
   post '/sessions', to: 'sessions#create', as: 'sessions'
   delete '/sessions', to: 'sessions#destroy', as: 'logout'
+  get 'about', to: 'static#about'
   #get "/results", to: "searches#show", as: 'results'
 
 end
