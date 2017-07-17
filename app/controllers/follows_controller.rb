769d71a95d096eb6d24ab51f730bbe560dcefb2e
n3m3sis42 <n3m3sis42@gmail.com>
2017-07-17 12:57:35 -0400

    added link to list of all users and made follow/unfollow redirect to list of friends, also added link to friends' searches

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,12 +3,12 @@ class FollowsController < ApplicationController
   def create
     @user = User.find(params[:following_id])
     current_user.follow(@user)
-    redirect_to user_path(@user)
+    redirect_to following_user_path(current_user)
   end
 
   def destroy
     @user = Follow.find(params[:id]).following
     current_user.unfollow(@user)
-    redirect_to user_path(@user)
+    redirect_to following_user_path(current_user)
   end
 end
